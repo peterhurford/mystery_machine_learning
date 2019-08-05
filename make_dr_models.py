118f615b9fe3c9b60c75cdd8a2cc12b444fc83d0
Peter Hurford <peter@peterhurford.com>
2019-08-05 12:18:40 -0700

    Fix dict bug and timeout bug

--- make_dr_models.py
+++ make_dr_models.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import datarobot as dr
 
+from collections import defaultdict
+
 from utils import CHARACTERS
 
 
 lines = pd.read_csv('scooby_doo_lines.csv')
-projects = {}
+projects = defaultdict(lambda: '')
 
 for character in CHARACTERS:
     print('Building {} model...'.format(character))
@@ -15,7 +17,7 @@ for character in CHARACTERS:
                                project_name=character,
                                target='target',
                                worker_count=-1)
-    project.wait_for_autopilot()
+    project.wait_for_autopilot(timeout=2*60*60*60)
     projects[character] = project.id
 import pdb
 pdb.set_trace()
