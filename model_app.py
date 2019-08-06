6bd3beba5bc73975e7a4279c61e7dfca1ee92c91
Peter Hurford <peter@peterhurford.com>
2019-08-05 23:18:06 -0500

    Make a flask app

---
+++ model_app.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from flask import Flask, jsonify
+from joblib import load
+from collections import defaultdict
+
+from utils import CHARACTERS, remove_parentheticals, clean_punct, print_step
+
+
+def predict_character(text):
+    t_text = remove_parentheticals(text)
+    t_text = clean_punct(t_text)
+    tfidf_text = tfidf.transform([t_text])
+    preds = defaultdict(lambda: 0)
+    for character in CHARACTERS:
+        preds[character] = models[character].predict_proba(tfidf_text)[:, 1][0]
+    sumx = sum(preds.values())
+    for character in CHARACTERS:
+        preds[character] /= sumx
+    return {'text': text,
+            'prediction': preds.keys()[np.argmax(preds.values())],
+            'predictions': preds}
+
+
+print_step('Load TFIDF')
+tfidf = load('cache/tfidf.joblib')
+models = defaultdict(lambda: '')
+for character in CHARACTERS:
+    print_step('Loading {} model...'.format(character))
+    models[character] = load('cache/{}_model.joblib'.format(character))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Main page!'
+
+@app.route('/ping')
+def ping():
+    return 'Pong'
+
+@app.route('/predict/<string>')
+def predict(string):
+    return jsonify(predict_character(string))
