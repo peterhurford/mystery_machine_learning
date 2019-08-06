6078326d8fd94ff8d883e1c4beb2906d7bfc739d
Peter Hurford <peter@peterhurford.com>
2019-08-06 12:26:27 -0500

    Add build to git

---
+++ client/that-demo/build/precache-manifest.ec503a805e40dd88946330b7f11ba82d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672e5db155fb7d23e59e2366dbc0f362",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254110fb602c41e49eab",
+    "url": "/static/css/main.747aacdc.chunk.css"
+  },
+  {
+    "revision": "3d6fce847343d36cd3c9",
+    "url": "/static/js/2.f6ee7308.chunk.js"
+  },
+  {
+    "revision": "254110fb602c41e49eab",
+    "url": "/static/js/main.9dae895a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d440af5789d2a1767975107dee93929c",
+    "url": "/static/media/Daphne_Blake.d440af57.png"
+  },
+  {
+    "revision": "345950e80bf6f9d71a5f6b9fcd03d0b3",
+    "url": "/static/media/Scooby_Doo.345950e8.png"
+  },
+  {
+    "revision": "75ea70d79bd65cda73b77d01b7810c3c",
+    "url": "/static/media/Shaggy_Rogers.75ea70d7.png"
+  },
+  {
+    "revision": "12cbc0668d9d5b89ea0e559fbf48c297",
+    "url": "/static/media/mystery_machine.12cbc066.png"
+  }
+]);
